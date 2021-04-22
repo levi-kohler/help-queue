5eacb409e0b58a0ab1ba6f9a839e668e9816a3f6
Levi Kohler <kohler.la01@gmail.com>
2021-04-21 19:11:42 -0700

    update render method to handle deleting ticket

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -51,9 +51,8 @@ class TicketControl extends React.Component {
     let buttonText = null; 
 
     if (this.state.selectedTicket != null) {
-      currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket} />
+      currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket} onClickingDelete = {this.handleDeletingTicket} />
       buttonText = "Return to Ticket List";
-
     }
     else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}  />;
