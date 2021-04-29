c12505a0cc3ac0bc5d4ea6c2263f04f100630994
Levi Kohler <kohler.la01@gmail.com>
2021-04-29 15:20:56 -0700

    try to fix bug where timer wont change

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -7,7 +7,7 @@ function TicketList(props){
     <React.Fragment>
       <hr />
       {/* We now need to map over the values of an object, not an array. */}
-      {Object.values(props.ticketList).map((ticket) =>
+      {Object.values(props.ticketList).map((ticket) => 
         <Ticket
           whenTicketClicked = { props.onTicketSelection }
           names={ticket.names}
@@ -16,7 +16,7 @@ function TicketList(props){
           formattedWaitTime={ticket.formattedWaitTime}
           id={ticket.id}
           key={ticket.id}/>
-      )}
+        )}
       {/* Don't forget to add the curly brace above - otherwise there will be a syntax error. */}
     </React.Fragment>
   );
