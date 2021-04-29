c469f6646129b9d1cd0c0fa2dfb182256ae8153c
Levi Kohler <kohler.la01@gmail.com>
2021-04-29 11:19:15 -0700

    add Reducer Logic to UPDATE_TIME

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -2,6 +2,8 @@ import ticketListReducer from '../../reducers/ticket-list-reducer';
 import * as c from './../../actions/ActionTypes';
 
 describe('ticketListReducer', () => {
+  
+  let action;
 
   const currentState = {
     1: {names: 'Ryan & Aimen',
@@ -14,11 +16,11 @@ describe('ticketListReducer', () => {
     id: 2 }
   }
 
-  let action;
   const ticketData = {
     names: 'Ryan & Aimen',
     location: '4b',
     issue: 'Redux action is not working correctly.',
+    timeOpen : 0,
     id: 1
     
   };
@@ -57,6 +59,25 @@ describe('ticketListReducer', () => {
         location: '2a',
         issue: 'Reducer has side effects.',
         id: 2 }
+    });
+  });
+
+  test('Should add a formatted wait time to ticket entry', () => {
+    const { names, location, issue, timeOpen, id } = ticketData;
+    action = {
+      type: c.UPDATE_TIME,
+      formattedWaitTime: '4 minutes',
+      id: id
+    };
+    expect(ticketListReducer({ [id] : ticketData }, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: '4 minutes'
+      }
     });
   });
 });
